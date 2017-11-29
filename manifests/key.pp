324d9546c67cacf5aa38ecef3656112e95535973
Fabian Zimmermann <fabian.zimmermann@noris.de>
2017-11-29 11:30:25 +0100

    key: added caps_{mds,mon,osd}-support

--- manifests/key.pp
+++ manifests/key.pp
@@ -7,13 +7,28 @@
 # [*key*]
 #   This is the key
 #
+# [*caps_mds*]
+# [*caps_mon*]
+# [*caps_osd*]
+#
 
-define ceph::key ($key){
+define ceph::key (
+    $key,
+    $auid = undef,
+    $caps_mds = undef,
+    $caps_mon = undef,
+    $caps_osd = undef
+    ){
   if $title == 'client.admin' {
     $_auid = 0
     $_caps_mds = 'allow'
     $_caps_mon = 'allow *'
     $_caps_osd = 'allow *'
+  } else {
+    $_auid = $auid
+    $_caps_mds = $caps_mds
+    $_caps_mon = $caps_mon
+    $_caps_osd = $caps_osd
   }
   concat::fragment { "/etc/ceph/keyring-${title}":
     target  => '/etc/ceph/keyring',
