48134d93dec9b763437d891bc4ec4faae13185ec
Fabian Zimmermann <fabian.zimmermann@noris.de>
2018-04-09 15:29:52 +0200

    use service-definition of rbdmap instead of exec

--- manifests/client/rbd.pp
+++ manifests/client/rbd.pp
@@ -2,12 +2,11 @@ define ceph::client::rbd ($pool=rbd,$id=admin) {
   concat::fragment{"/etc/ceph/rbdmap-${title}":
     target  => '/etc/ceph/rbdmap',
     content => "${pool}/${title}	id=${id},keyring=/etc/ceph/keyring\n",
-  }
+  } ~>
 
-  exec { "map-rbd-${title}":
-    command => "rbd -p ${pool} --id ${id} map ${title}",
-    path    => [ '/usr/bin/', '/bin/' ],
-    creates => "/dev/rbd/${pool}/${title}",
+  service{ 'rbdmap':
+    ensure => 'running',
+    enable => true,
     require => Package['ceph']
   }
 
